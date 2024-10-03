d28a058a6833c058aa534dfef89ddfa43b506679
Evan Franciszok <git@evan.mailtje.net>
2024-10-03 10:27:09 +0200

    working creation of the canbus and config classes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+#include <iostream>
+#include <iomanip>
+#include "lib/communication/canbus.h"
+#include <libconfig.h++>
+#include "lib/communication/CANOpenMaster.h"
+
+int main() {
+    std::cout << R"(
+   ____     __         ___            __ ______                __
+  / __/__  / /__ _____/ _ )___  ___ _/ //_  __/    _____ ___  / /____
+ _\ \/ _ \/ / _ `/ __/ _  / _ \/ _ `/ __// / | |/|/ / -_) _ \/ __/ -_)
+/___/\___/_/\_,_/_/ /____/\___/\_,_/\__//_/  |__,__/\__/_//_/\__/\__/
+)"
+    << std::endl;
+    Config cfg("../boat.cfg");
+    CanBus can_control(cfg.read("can.control"));
+
     return 0;
 }
